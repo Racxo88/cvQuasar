a6fa11c2ac696d2456fd855c315be603ace1c72a
Oscar García-Olalla Olivera <oscar.olalla@xeridia.com>
2017-04-25 16:13:19 +0200

    First commit

---
+++ src/components/Home.vue
@@ -0,0 +1,62 @@
+<template>
+
+ <q-layout>
+  <!-- Header -->
+  <div slot="header" class="toolbar">
+    <!-- opens left-side drawer using its ref -->
+    <button class="hide-on-drawer-visible" @click="$refs.leftDrawer.open()">
+      <i>menu</i>
+    </button>
+    <q-toolbar-title :padding="3">
+      Title
+    </q-toolbar-title>
+    <!-- opens right-side drawer using its ref -->
+    <button class="hide-on-drawer-visible" @click="$refs.rightDrawer.open()">
+        <i>menu</i>
+      </button>
+  </div>
+  <!-- Navigation Tabs -->
+  <q-tabs slot="navigation">
+    <q-tab icon="mail" route="/layout" exact replace>Mails</q-tab>
+    <q-tab icon="alarm" route="/layout/alarm" exact replace>Alarms</q-tab>
+    <q-tab icon="help" route="/layout/help" exact replace>Help</q-tab>
+  </q-tabs>
+  <!-- Left-side Drawer -->
+  <q-drawer ref="leftDrawer">
+    <div class="list no-border platform-delimiter">
+      <q-drawer-link  icon="mail" :to="{path: 'Error404', exact: true}">
+      Sent mail
+      </q-drawer-link>
+    </div>
+  </q-drawer>
+  <!-- IF USING subRoutes only: -->
+  <router-view class="layout-view"></router-view>
+  <!-- OR ELSE, IF NOT USING subRoutes: -->
+  <div class="layout-view"></div>
+  <!-- Right-side Drawer -->
+  <q-drawer ref="rightDrawer" :right-side="true">
+  <div class="list platform-delimiter">
+    <div class="item">
+      <div class="item-content">
+        List Item
+      </div>
+    </div>
+    
+  </div>
+  </q-drawer>
+  <!-- Footer -->
+  <div slot="footer" class="toolbar">
+  </div>
+</q-layout>
+</template>
+<script>
+export default {
+  methods: {
+  }
+}
+</script>
+
+
+<<style lang="styl">
+  
+</style>
---
+++ src/components/Login.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+      <div class="layout-padding">
+        <div class="column gutter items-center justify-center">
+          <div>
+            <div class="card ">
+              <div class="card-title">
+                <h2> Login </h2>
+              </div>
+              <div class="card-content">
+                <div class="entry">
+                  <div class="floating-label">
+                    <input v-model="userName" required class="full-width">
+                    <label class="text-primary">User Name</label>
+                  </div>
+                </div>
+                <div class="entry">
+                  <div class="floating-label">
+                    <input v-model="password" type="password" required class="full-width">
+                    <label class="text-primary">Password</label>
+                 </div>
+                </div>
+              </div>
+            </div> 
+          </div>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+  
+</script>
+
+<style lang="styl">
+.h2{
+  justify-content:center
+
+}
+.layout-padding{
+  height:100vh
+  min-height:100vh
+  background-image: url(../assets/loginBackground.jpg)
+  background-size:50%
+  margin-top:-25px
+}
+.card{
+  height:35em
+  width:20em
+  background-image: url(../assets/loginCard.png)
+  background-size:20em 35em;
+  backgrount-repeat:no-repeat
+  border:0px
+  box-shadow:none
+}
+.card-title{
+  text-align: center
+ }
+.entry
+{
+  padding-right: 35 px
+  padding-top:7px
+  padding-left: 35 px
+  padding-bottom:10px
+}
+</style>
