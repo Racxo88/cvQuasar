8c62862e473e6b51491abdc7a3673c569c853055
Oscar García-Olalla Olivera <oscar.olalla@xeridia.com>
2017-05-19 13:50:23 +0200

    university courses without db

--- src/components/University.vue
+++ src/components/University.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="column full-width " id="flat">
-    <div class="card MyDegrees row width-1of2  full-width">
+    <div class="card MyDegrees row width-1of2 full-width">
       <div class="card-content column full-width">
         <div class="Headers row bg-primary">
           <label class="text-light width-2of5">Name</label>
           <label class="text-light width-2of5">Status</label>
           <label class="text-light width-1of5"></label>
         </div>
-        <div class="Degrees column align-start full-height scroll">
+        <div class="Degrees column align-start full-height scroll ">
           <div class="oneDegree row justify-around items-center items-center ">
             <label class="text-light width-2of5">Filology</label>
             <label class="text-light width-2of5">On Progess</label>
@@ -77,15 +77,29 @@
       </div>
     </div>
     <div class="card OtherDegrees width-1of2 row full-width">
-      <div class="card-content column full-width">
-        <q-slider infinite class="text-white">
-          <div slot="slide" class="bg-primary centered">
-            Slide 1
+      <div class="card-content column full-width full-height justify-center">
+        <q-slider infinite  actions class="text-white full-width full-height">
+          <div slot="slide" class="centered full-width text-black" id="dragon">
+            Dragon Ryder
           </div>
-          <div slot="slide" class="bg-secondary centered">
-            Slide 2
+          <div slot="slide" class="centered full-width text-black" id="chino">
+            Chinese Maestry
           </div>
+           <div slot="slide" class="centered full-width text-light" id="alchemy">
+            Alchemy
+          </div>
+          <div class="q-slider-toolbar row justify-end ">
           
+          <div  slot="action" class="moneyDegree row items-center">
+          <img src='../assets/Icons/monedas.png'>1000</img>
+          </div>
+          <i slot="action" @click="someMethod()">
+            add_shopping_cart
+          </i>
+          <i slot="action" @click="someMethod()">
+            info
+          </i>
+          </div>
         </q-slider>
       </div>
     </div>
@@ -112,7 +126,7 @@ export default {
     border-bottom:0px
 }
 .Degrees{
-  background-color: rgba(FF, FF, FF, 55);
+  background-color: rgba(255, 255, 255, 0.5);
   border: 2px solid whitesmoke;
   border-top:0px
 }
@@ -121,5 +135,44 @@ export default {
 }
 label{
   font-size:1em
+}
+.moneyDegree{
+  margin-right:1em
+}
+#dragon{
+  background: url(../assets/Degrees/dragon.png);
+    background-repeat:repeat;
+    background-size: cover;
+    background-position: center;
+    font-size:12vw;
+    font-weight: bold;
+    border: 2px solid whitesmoke;
+}
+#chino{
+  background: url(../assets/Degrees/chino.png);
+    background-repeat:repeat;
+    background-size: cover;
+    background-position: center;
+    font-size:10vw;
+    font-weight: bold;
+    border: 2px solid whitesmoke;
+}
+#alchemy{
+  background: url(../assets/Degrees/alchemy.jpg);
+    background-repeat:repeat;
+    background-size: contain;
+    background-position: center;
+    font-size:12vw;
+    font-weight: bold;
+    border: 2px solid whitesmoke;
+}
+
+div .q-slider-toolbar{
+opacity: 1;
+padding:0
+}
+.card-content{
+  padding-top:0.3em;
+  padding-bottom:0px
 }
 </style>
